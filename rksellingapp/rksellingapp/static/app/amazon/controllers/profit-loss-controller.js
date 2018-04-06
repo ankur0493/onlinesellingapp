42ec17218d675634f8719afd6ea4239581797ec9
Ankur Gupta <ankur@retechlabs.com>
2018-04-06 08:42:48 +0530

    Alert if mandatory fields not filled in

--- rksellingapp/rksellingapp/static/app/amazon/controllers/profit-loss-controller.js
+++ rksellingapp/rksellingapp/static/app/amazon/controllers/profit-loss-controller.js
@@ -9,10 +9,15 @@
                 $scope.amazonFormData = {};
 
                 $scope.submitAmazonForm = function() {
+                    var formData = $scope.amazonFormData;
+                    if(!(formData.product_name && formData.purchase_price_with_gst && formData.weight && formData.region && formData.list_price)) {
+                        alert("Please fill all fields marked with *")
+                    }
                     $http.post('amazon/profit-loss/', $scope.amazonFormData)
                     .then(function(response) {
                         $scope.amazonFormData = response.data;
                         },function(error) {
+                            // console.log(error);
                     });
                 };
 
